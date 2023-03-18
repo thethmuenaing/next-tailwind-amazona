8e61110a35b2d83afc6ba04e09e32a3b389df8f4
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-18 23:54:26 +0630

    check out button

--- pages/cart.js
+++ pages/cart.js
@@ -2,10 +2,12 @@ import Layout from "@/components/Layout";
 import { Store } from "@/utils/Store";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useContext } from "react";
 
 export default function CartScreen() {
 	const { state, dispatch } = useContext(Store);
+	const router = useRouter();
 
 	const {
 		cart: { cartItems },
@@ -74,6 +76,24 @@ export default function CartScreen() {
 								))}
 							</tbody>
 						</table>
+					</div>
+					<div className="card p-5">
+						<ul>
+							<li>
+								<div className="pb-3 text-xl">
+									Subtotal ( {cartItems.reduce((a, c) => a + c.quantity, 0)} ) :
+									$ {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+								</div>
+							</li>
+							<li>
+								<button
+									onClick={() => router.push("/shipping")}
+									className="primary-button w-full"
+								>
+									Check Out
+								</button>
+							</li>
+						</ul>
 					</div>
 				</div>
 			)}
