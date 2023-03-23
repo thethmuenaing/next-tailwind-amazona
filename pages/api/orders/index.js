dca11fdd73aad81bcbd2fb34a29aedcc1536a01b
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-23 10:31:26 +0630

    Create Place Order Screen

---
+++ pages/api/orders/index.js
@@ -0,0 +1,20 @@
+import Order from "@/models/Order";
+import db from "@/utils/db";
+import { getSession } from "next-auth/react";
+
+const handler = async (req, res) => {
+	const session = await getSession({ req });
+
+	if (!session) {
+		return res.status(401).send("signin required");
+	}
+	const { user } = session;
+	await db.connect();
+	const newOrder = new Order({
+		...req.body,
+		user: user._id,
+	});
+	// const order = await newOrder.save();
+	res.status(201).send(newOrder);
+};
+export default handler;
