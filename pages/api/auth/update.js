d5e93c10693a19659002a2323bc37a1e4688c2ff
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-27 23:05:55 +0630

    Update user profile

---
+++ pages/api/auth/update.js
@@ -0,0 +1,50 @@
+import User from "@/models/User";
+import bcryptjs from "bcryptjs";
+import db from "@/utils/db";
+import { getSession } from "next-auth/react";
+
+async function handler(req, res) {
+	if (req.method !== "PUT") {
+		return res.status(400).send({ message: `${req.method} not supported` });
+	}
+
+	const session = await getSession({ req });
+	if (!session) {
+		return res.status(401).send({ message: "signin required" });
+	}
+
+	const { user } = session;
+	console.log("user ", user);
+	const userId = await User.findOne({ email: user.email });
+	const { name, email, password } = req.body;
+
+	if (
+		!name ||
+		!email ||
+		!email.includes("@") ||
+		(password && password.trim().length < 5)
+	) {
+		res.status(422).json({
+			message: "Validation error",
+		});
+		return;
+	}
+
+	await db.connect();
+	const toUpdateUser = await User.findById(userId._id);
+	console.log("loUpdateUser ", toUpdateUser);
+	toUpdateUser.name = name;
+	toUpdateUser.email = email;
+
+	if (password) {
+		toUpdateUser.password = bcryptjs.hashSync(password);
+	}
+
+	await toUpdateUser.save();
+	await db.disconnect();
+	res.send({
+		message: "User updated",
+	});
+}
+
+export default handler;
